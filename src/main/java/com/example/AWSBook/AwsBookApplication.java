7cf8b3a6dcd3a7e09e2ad4d225ef59f6602e7f55
moong <moongg0@hotmail.com>
2022-11-06 14:57:16 +0900

    1st commit

---
+++ src/main/java/com/example/AWSBook/AwsBookApplication.java
@@ -0,0 +1,13 @@
+package com.example.AWSBook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsBookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsBookApplication.class, args);
+	}
+
+}
