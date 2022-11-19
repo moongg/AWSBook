3af8fe9485b9803b28219f2ee18b046195aac9a8
moong <moongg0@hotmail.com>
2022-11-20 04:37:42 +0900

    2022.11.19 junit5 lombok Tested-2

--- src/main/java/com/example/AWSBook/web/HelloController.java
+++ src/main/java/com/example/AWSBook/web/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.AWSBook.web;
 
+import com.example.AWSBook.web.dto.HelloResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,5 +10,10 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello(@RequestParam String user) {
         return "Hello "+user+", Have a Hard Work!!!";
+    }
+
+    @GetMapping("/hello/dto")
+    public HelloResponseDto helloDto(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+        return new HelloResponseDto(name, amount);
     }
 }
--- src/test/java/com/example/AWSBook/web/HelloControllerTest.java
+++ src/test/java/com/example/AWSBook/web/HelloControllerTest.java
@@ -1,5 +1,6 @@
 package com.example.AWSBook.web;
 
+import org.json.JSONObject;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,7 @@ import org.springframework.boot.test.web.client.TestRestTemplate;
 import java.net.URI;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class HelloControllerTest {
@@ -32,4 +34,28 @@ public class HelloControllerTest {
         System.out.println(message);
         assertEquals(expectedValue, message);
     }
+    @Test
+    @DisplayName("-- Hello + dto가 리턴됩니다. --")
+    public void helloResponseDtoTest() throws Exception {
+        String name = "홍길동";
+        int amount = 9876;
+
+        URI targetUrl = UriComponentsBuilder.fromUriString("/hello/dto")
+                .queryParam("name", name)
+                .queryParam("amount", amount)
+                .build()
+                .encode()
+                .toUri();
+
+        String message = this.restTemplate.getForObject(targetUrl, String.class);
+
+        JSONObject obj = new JSONObject(message);
+        String returnName = obj.get("name").toString();
+        int returnAmount = Integer.parseInt(obj.get("amount").toString());
+
+        assertEquals(name, returnName);
+        assertEquals(amount, returnAmount);
+    }
+
+
 }
