127857018f809f6831737f6857af8b1ab7cf4c78
moong <moongg0@hotmail.com>
2022-11-19 11:23:14 +0900

    2022.11.19 commit

--- src/main/java/com/example/AWSBook/AwsBookApplication.java
+++ src/main/java/com/example/AWSBook/AwsBookApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AwsBookApplication {
 
+	// Comment 한글 코멘트 추가
 	public static void main(String[] args) {
 		SpringApplication.run(AwsBookApplication.class, args);
 	}
